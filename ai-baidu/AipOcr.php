f06e9937319865b1e80f6ad7f5db422b67f150ef
许许恒 <1197256140@qq.com>
2019-02-22 09:28:09 +0800

    a

--- ai-baidu/AipOcr.php
+++ ai-baidu/AipOcr.php
@@ -14,7 +14,7 @@
 * License for the specific language governing permissions and limitations under
 * the License.
 */
-
+namespace Aip;
 require_once 'lib/AipBase.php';
 
 class AipOcr extends AipBase {
--- ai-baidu/lib/AipBase.php
+++ ai-baidu/lib/AipBase.php
@@ -14,6 +14,7 @@
 * License for the specific language governing permissions and limitations under
 * the License.
 */
+namespace Aip;
 
 require_once 'AipHttpClient.php';
 require_once 'AipBCEUtil.php';
